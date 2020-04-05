af2ec9b605ff7d9da0cd4c4728d21a74f1fd19fe
John Batson <batson@icl.utk.edu>
2020-04-04 22:07:12 -0400

    rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,9 @@ release = '0.0.1'
 import sys, os
 sys.path.append(os.path.abspath('extensions'))
 
-extensions = ['sphinxcontrib.bibtex']
+import sphinx_rtd_theme
+
+extensions = ['sphinxcontrib.bibtex', "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -49,7 +51,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
