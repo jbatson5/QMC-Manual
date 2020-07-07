7fdba6cb1d2bfecb738900de7eb4e92a8ebbee65
John Batson <batson@icl.utk.edu>
2020-07-06 22:19:57 -0400

    finishing manual

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, QMCPACK Developers'
 author = 'QMCPACK Developers'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+
 
 
 # -- General configuration ---------------------------------------------------
